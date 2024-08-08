5bdd65aab8150465033e51830d49266ea9f77d89
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-08 17:30:47 +0200

    Organisation fichiers

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = "Salut";
+let b = a;
+const isMajeur = undefined;
+const notes = [13, 14, 8, 9, "hello", [1, 2, 3]];
+const notes2 = notes;
+const person = {
+  firstname: "John",
+  lastname: "Doe",
+  age: 24,
+  notes: [12, 14, 15],
+  job: {
+    name: "informaticien",
+    hours: 35,
+  },
+  [b]: 23,
+};
