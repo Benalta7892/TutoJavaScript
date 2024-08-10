d8195d83bd0e8b92752eb029474c5c2d94fde1a5
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 13:04:57 +0200

    Conditions et opérateurs logiques

--- 2_conditions/script.js
+++ 2_conditions/script.js
@@ -1,28 +1,40 @@
-if (true) {
-  console.log("Bonjour");
-} // "Bonjour"
+// if (true) {
+//   console.log("Bonjour");
+// } // "Bonjour"
 
-if (false) {
-  console.log("Bonjour");
-} // N'affiche rien
+// if (false) {
+//   console.log("Bonjour");
+// } // N'affiche rien
 
+// const age = 20;
+// if (age > 18) {
+//   console.log("Vous êtes majeur");
+// } // "Vous êtes majeur"
+
+// if (age >= 18) {
+//   console.log("Vous êtes majeur");
+// } // "Vous êtes majeur"
+
+// if (age < 18) {
+//   console.log("Vous êtes mineur");
+// } // N'affiche rien
+
+// if (age == 18) {
+//   console.log("Vous avez 18 ans");
+// } // N'affiche rien
+
+// if (age != 18) {
+//   console.log("Vous n'avez pas 18 ans");
+// } // "Vous n'avez pas 18 ans"
+
+// Opérateurs logiques
 const age = 20;
-if (age > 18) {
-  console.log("Vous êtes majeur");
-} // "Vous êtes majeur"
-
-if (age >= 18) {
-  console.log("Vous êtes majeur");
-} // "Vous êtes majeur"
-
-if (age < 18) {
-  console.log("Vous êtes mineur");
-} // N'affiche rien
-
-if (age == 18) {
-  console.log("Vous avez 18 ans");
-} // N'affiche rien
-
-if (age != 18) {
-  console.log("Vous n'avez pas 18 ans");
-} // "Vous n'avez pas 18 ans"
+const pays = "FR";
+// if ((pays === "FR" && age >= 18) || (pays === "US" && age >= 16)) {
+//   console.log("Vous avez le droit de conduire");
+// } // "Vous avez le droit de conduire"
+const peutConduireFrance = pays === "FR" && age >= 18;
+const peutConduireUS = pays === "US" && age >= 16;
+if (peutConduireFrance || peutConduireUS) {
+  console.log("Vous avez le droit de conduire");
+} // "Vous avez le droit de conduire"
