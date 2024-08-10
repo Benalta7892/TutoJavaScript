eddaca8fe15e2dde1a4168aef3118ab16569fb6d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 16:11:22 +0200

    Cours sur les boucles

--- 4_boucles/script.js
+++ 4_boucles/script.js
@@ -18,23 +18,32 @@
 //   console.log(notes[i]);
 // }
 
-// // La boucle for ... in, pour parcourir par les index des éléments d'un tableau
+// // La boucle for ... in, pour parcourir par les index / clefs d'un objet ou d'un tableau
 // const notes = [12, 13, 16, 8];
 // for (let i in notes) {
 //   console.log(notes[i]);
 // }
 
-// // La boucle for ... of, pour parcourir les valeurs d'un tableau
-const notes = [12, 13, 16, 8];
-const person = {
-  firsname: "John",
-  lastname: "Doe",
-};
-for (let note of notes) {
-  console.log(note);
-} // 12 13 16 8
-for (let note of person) {
-  console.log(note);
-} // Il y aura une erreur car on ne peut pas utiliser for ... of sur un objet (person)
-// mais sur un tableau (notes) ou une chaine de caractères (string), ce n'est pas itérable
-// le for ... in est utilisé pour les objets itérables (tableaux, chaines de caractères)
+// // La boucle for ... of, pour parcourir (itérable) les valeurs d'un tableau
+// const notes = [12, 13, 16, 8];
+// const person = {
+//   firsname: "John",
+//   lastname: "Doe",
+// };
+// for (let note of notes) {
+//   console.log(note);
+// } // 12 13 16 8
+// for (let note of person) {
+//   console.log(note);
+// } // Il y aura une erreur car on ne peut pas utiliser for ... of sur un objet (person)
+// // mais sur un tableau (notes) ou une chaine de caractères (string), ce n'est pas itérable
+// // le for ... in est utilisé pour les objets itérables (tableaux, chaines de caractères)
+// const greeting = "Bonjour";
+// for (let letter of greeting) {
+//   console.log(letter);
+// } // B o n j o u r
+
+const greeting = "Bonjour";
+for (let letter in greeting) {
+  console.log(letter);
+} // 0 1 2 3 4 5 6
