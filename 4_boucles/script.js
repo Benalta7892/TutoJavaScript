f4424a0486066f2de003c8eac48d8b50aea14e6d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 16:06:38 +0200

    Boucles for ... of et for ... in

--- 4_boucles/script.js
+++ 4_boucles/script.js
@@ -13,7 +13,28 @@
 // }
 
 // // La boucle for, peu être utilisée pour itérer et parcourir les éléments d'un tableaux
+// const notes = [12, 13, 16, 8];
+// for (let i = 0; i < notes.length; i++) {
+//   console.log(notes[i]);
+// }
+
+// // La boucle for ... in, pour parcourir par les index des éléments d'un tableau
+// const notes = [12, 13, 16, 8];
+// for (let i in notes) {
+//   console.log(notes[i]);
+// }
+
+// // La boucle for ... of, pour parcourir les valeurs d'un tableau
 const notes = [12, 13, 16, 8];
-for (let i = 0; i < notes.length; i++) {
-  console.log(notes[i]);
-}
+const person = {
+  firsname: "John",
+  lastname: "Doe",
+};
+for (let note of notes) {
+  console.log(note);
+} // 12 13 16 8
+for (let note of person) {
+  console.log(note);
+} // Il y aura une erreur car on ne peut pas utiliser for ... of sur un objet (person)
+// mais sur un tableau (notes) ou une chaine de caractères (string), ce n'est pas itérable
+// le for ... in est utilisé pour les objets itérables (tableaux, chaines de caractères)
