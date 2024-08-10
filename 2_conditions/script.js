091fa88ce8f7a57ebbb0c7e56f9d17d10f450dd9
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 13:20:29 +0200

    Inversion des conditions

--- 2_conditions/script.js
+++ 2_conditions/script.js
@@ -28,17 +28,41 @@
 // } // "Vous n'avez pas 18 ans"
 
 // Opérateurs logiques
-const age = 20;
-const pays = "FR";
+// const age = 20;
+// const pays = "FR";
+// // if ((pays === "FR" && age >= 18) || (pays === "US" && age >= 16)) {
+// //   console.log("Vous avez le droit de conduire");
+// // } // "Vous avez le droit de conduire"
+// const peutConduireFrance = pays === "FR" && age >= 18;
+// const peutConduireUS = pays === "US" && age >= 16;
+// if (peutConduireFrance || peutConduireUS) {
+//   console.log("Vous avez le droit de conduire en France");
+// } else if (peutConduireUS) {
+//   console.log("Vous avez le droit de conduire aux US");
+// } else {
+//   console.log("Vous n'avez pas le droit de conduire");
+// }
+
+// Opérateurs inversions
+const age = 13;
+const pays = "US";
 // if ((pays === "FR" && age >= 18) || (pays === "US" && age >= 16)) {
 //   console.log("Vous avez le droit de conduire");
 // } // "Vous avez le droit de conduire"
 const peutConduireFrance = pays === "FR" && age >= 18;
 const peutConduireUS = pays === "US" && age >= 16;
-if (peutConduireFrance || peutConduireUS) {
-  console.log("Vous avez le droit de conduire en France");
-} else if (peutConduireUS) {
-  console.log("Vous avez le droit de conduire aux US");
-} else {
+
+// a || b => !a && !b
+// a && b => !a || !b
+
+// if (!peutConduireFrance || !peutConduireUS) {
+//   console.log("Vous n'avez pas le droit de conduire");
+// }
+
+// if (!(pays === "FR" && age >= 18) && !(pays === "US" && age >= 16)) {
+//   console.log("Vous n'avez pas le droit de conduire");
+// } // "Vous n'avez pas le droit de conduire"
+
+if ((pays !== "FR" || age < 18) && (pays !== "US" || age < 16)) {
   console.log("Vous n'avez pas le droit de conduire");
-}
+} // "Vous n'avez pas le droit de conduire"
