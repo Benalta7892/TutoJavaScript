3cbeec334590dcc7b38b4bab1aedce54232152de
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 15:57:42 +0200

    Itérattion avec la boucle for

--- 4_boucles/script.js
+++ 4_boucles/script.js
@@ -1,8 +1,19 @@
 // Les boucles
 
-// La boucle tant que (while)
-let i = 0;
-while (i < 10) {
-  console.log("Bonjour" + i);
-  i = i + 1;
+// // La boucle tant que (while) est utilisée pour répéter une action tant qu'une condition est vraie
+// let i = 0;
+// while (i < 10) {
+//   console.log("Bonjour" + i);
+//   i = i + 1;
+// }
+
+// //La boucle pour (for), pour executer une itération certain nombre de fois
+// for (let i = 0; i < 10; i++) {
+//   console.log("Bonjour");
+// }
+
+// // La boucle for, peu être utilisée pour itérer et parcourir les éléments d'un tableaux
+const notes = [12, 13, 16, 8];
+for (let i = 0; i < notes.length; i++) {
+  console.log(notes[i]);
 }
