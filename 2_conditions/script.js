63e0375631685eccad05eab76550d701b6e44903
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 13:38:21 +0200

    Exercices sur les conditions

--- 2_conditions/script.js
+++ 2_conditions/script.js
@@ -67,20 +67,36 @@
 //   console.log("Vous n'avez pas le droit de conduire");
 // } // "Vous n'avez pas le droit de conduire"
 
-// Conditions sur variables
-const age = 17;
-const pays = "FR";
-const peutConduireFrance = pays === "FR" && age >= 18;
-const peutConduireUS = pays === "US" && age >= 16;
+// // Conditions sur variables
+// const age = 17;
+// const pays = "FR";
+// const peutConduireFrance = pays === "FR" && age >= 18;
+// const peutConduireUS = pays === "US" && age >= 16;
 
-switch (pays) {
-  case "FR":
-    console.log("Vous êtes en France");
-    break;
-  case "US":
-    console.log("Vous êtes aux US");
-    break;
-  default:
-    console.log("Vous êtes dans un autre pays");
-    break;
-} // "Vous êtes en France"
+// switch (pays) {
+//   case "FR":
+//     console.log("Vous êtes en France");
+//     break;
+//   case "US":
+//     console.log("Vous êtes aux US");
+//     break;
+//   default:
+//     console.log("Vous êtes dans un autre pays");
+//     break;
+// } // "Vous êtes en France"
+
+// Exercices : Proposer un film en fonction de l'âge
+
+// Demander l'âge de l'utilisateur
+const age = prompt("Quel est votre âge ?");
+
+if (age <= 13) {
+  // Si l'âge est inférieur ou égal à 13 ans alors afficher "Lilo et Stitch"
+  console.log("Lilo et Stitch");
+} else if (age <= 18) {
+  // Sinon si l'âge est inférieur ou égal à 18 ans alors afficher "Matrix"
+  console.log("Matrix");
+} else {
+  // Sinon afficher "Evil Dead"
+  console.log("Evil Dead");
+}
