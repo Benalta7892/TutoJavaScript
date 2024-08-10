c04fc9091797af709f13986ffcb7467672e19fc1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-10 12:47:41 +0200

    Conditions et opérateurs de comparaison

--- 2_conditions/script.js
+++ 2_conditions/script.js
@@ -5,3 +5,28 @@ if (true) {
 if (false) {
   console.log("Bonjour");
 } // N'affiche rien
+
+const age = 20;
+if (age > 18) {
+  console.log("Vous êtes majeur");
+} // "Vous êtes majeur"
+
+if (age >= 18) {
+  console.log("Vous êtes majeur");
+} // "Vous êtes majeur"
+
+if (age < 18) {
+  console.log("Vous êtes mineur");
+} // N'affiche rien
+
+if (age == 18) {
+  console.log("Vous avez 18 ans");
+} // N'affiche rien
+
+if (age != 18) {
+  console.log("Vous n'avez pas 18 ans");
+} // "Vous n'avez pas 18 ans"
+
+// git add .
+// git commit -m "Conditions et opérateurs de comparaison"
+// git push origin master
