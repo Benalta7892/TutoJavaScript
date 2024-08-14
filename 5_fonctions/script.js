438c79892309a231168d4d7e850b46f2dd517fd2
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-14 16:11:08 +0200

    Declaration d'uen fonction

--- 5_fonctions/script.js
+++ 5_fonctions/script.js
@@ -1,1 +1,10 @@
 // Les fonctions
+
+function canDrive(age, pays) {
+  if ((age >= 18 && pays === "FR") || (age >= 16 && pays === "US")) {
+    return true;
+  }
+  return false;
+}
+
+console.log(canDrive(17, "US")); // True
