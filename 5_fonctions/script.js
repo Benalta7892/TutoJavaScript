18390b96c04da908886f75b90f85bf9fee79c689
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-14 17:13:51 +0200

    Les fonctions, suite

--- 5_fonctions/script.js
+++ 5_fonctions/script.js
@@ -1,10 +1,17 @@
-// Les fonctions
+// // Les fonctions
 
-function canDrive(age, pays) {
-  if ((age >= 18 && pays === "FR") || (age >= 16 && pays === "US")) {
-    return true;
-  }
-  return false;
+// function canDrive(age, pays) {
+//   if ((age >= 18 && pays === "FR") || (age >= 16 && pays === "US")) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(canDrive(17, "US")); // true
+
+function greeting(name) {
+  console.log(`Bonjour ${name}`);
 }
 
-console.log(canDrive(17, "US")); // True
+greeting("Alice"); // Bonjour Alice
+greeting("Bob"); // Bonjour Bob
