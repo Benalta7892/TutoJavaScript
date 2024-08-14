67de6af313c6e9a849b2076d26cda1d887ab2d5e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-14 17:18:57 +0200

    Les fonctions, suite

--- 5_fonctions/script.js
+++ 5_fonctions/script.js
@@ -9,14 +9,23 @@
 
 // console.log(canDrive(17, "US")); // true
 
-let i = 0;
-function greeting(name) {
-  i++;
-  console.log(`Bonjour ${name}`);
+// let i = 0;
+// function greeting(name) {
+//   i++;
+//   console.log(`Bonjour ${name}`);
+// }
+
+// console.log(i); // 0
+// greeting("Alice"); // Bonjour Alice
+// console.log(i); // 1
+// greeting("Bob"); // Bonjour Bob
+// console.log(i); // 2
+
+let notes = [12, 13, 14];
+
+function upNotes(notes) {
+  notes[0]++;
 }
 
-console.log(i); // 0
-greeting("Alice"); // Bonjour Alice
-console.log(i); // 1
-greeting("Bob"); // Bonjour Bob
-console.log(i); // 2
+upNotes(notes);
+console.log(notes); // [13, 13, 14]
