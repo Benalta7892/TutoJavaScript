b0b83e93c439c4119b19714b99166518c55f6849
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-20 12:06:20 +0200

    Les fonctions, exercices, suite

--- 5_fonctions/script.js
+++ 5_fonctions/script.js
@@ -71,26 +71,45 @@
 // isRight(n)
 // guess()
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-const solution = getRandomInt(10);
-console.log(solution);
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * (max + 1));
+// }
+// const solution = getRandomInt(10);
+// console.log(solution);
 
-function isRight(n) {
-  return solution === n;
-}
+// function isRight(n) {
+//   return solution === n;
+// }
 
-function guess() {
-  const number = prompt("Entrez un chiffre") * 1;
-  return isRight(number);
-}
+// function guess() {
+//   const number = prompt("Entrez un chiffre") * 1;
+//   return isRight(number);
+// }
 
-for (i = 0; i < 3; i++) {
-  if (guess()) {
-    console.log("Bravo");
-    break;
-  } else if (i === 2) {
-    console.log("Perdu");
+// for (i = 0; i < 3; i++) {
+//   if (guess()) {
+//     console.log("Bravo");
+//     break;
+//   } else if (i === 2) {
+//     console.log("Perdu");
+//   }
+// }
+
+function isPremier(n) {
+  if (n < 2) {
+    return false;
   }
+  for (let i = n - 1; i > 1; i--) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log("0", isPremier(0));
+console.log("1", isPremier(1));
+console.log("2", isPremier(2));
+console.log("3", isPremier(3));
+console.log("11", isPremier(11));
+console.log("12", isPremier(12));
