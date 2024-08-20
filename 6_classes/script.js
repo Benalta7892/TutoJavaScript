f688441eebd286689805e7039300b684307a59a7
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-20 15:44:50 +0200

    Les classes

--- 6_classes/script.js
+++ 6_classes/script.js
@@ -1,3 +1,11 @@
+const moyenne = (notes) => {
+  let sum = 0;
+  for (let note of notes) {
+    sum = note + sum;
+  }
+  return sum / notes.length;
+};
+
 class Student {
   ecole = "Jules Ferry";
 
@@ -5,9 +13,18 @@ class Student {
     this.firstname = firstname;
     this.lastname = lastname;
   }
+
+  setNotes(notes) {
+    this.notes = notes;
+  }
+
+  canPass() {
+    return moyenne(this.notes) >= 10;
+  }
 }
 
 const John = new Student("John", "Doe");
 const Jane = new Student("Jane", "Doe");
-
-console.log(John, Jane);
+John.setNotes([10, 10, 9]);
+Jane.setNotes([15, 18, 19]);
+console.log(John.canPass(), Jane.canPass());
