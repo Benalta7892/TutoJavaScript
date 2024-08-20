f231c535bb10db910d6964c6179bdacf5588ddc8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-20 15:51:28 +0200

    Les classes, les getters

--- 6_classes/script.js
+++ 6_classes/script.js
@@ -21,6 +21,10 @@ class Student {
     }
   }
 
+  get name() {
+    return `${this.firstname} ${this.lastname}`;
+  }
+
   canPass() {
     return moyenne(this._notes) >= 10;
   }
@@ -30,4 +34,4 @@ const John = new Student("John", "Doe");
 const Jane = new Student("Jane", "Doe");
 John.notes = [19];
 Jane.notes = [15, 18, 19];
-console.log(John.canPass(), Jane.canPass());
+console.log(John.name);
