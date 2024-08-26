83740744e4fbaeec4c33a21b4f60c6ea7e5f6d87
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-26 18:06:23 +0200

    Sucre syntaxique

--- 9_sucre_syntaxique/script.js
+++ 9_sucre_syntaxique/script.js
@@ -42,5 +42,12 @@ console.log(premier, reste); // Affiche 12 [17, 18, 20]
 // const person1 = { firstname: "John", lastname: "Doe", age: 18 };
 // const firstname = person1.firstname; // Pas tres pratique
 
-const { firstname, lastname } = { firstname: "John", lastname: "Doe", age: 18 };
-console.log(firstname, lastname); // Affiche John Doe
+// const { firstname, lastname } = { firstname: "John", lastname: "Doe", age: 18 };
+// console.log(firstname, lastname); // Affiche John Doe
+
+const { firstname, ...reste1 } = {
+  firstname: "John",
+  lastname: "Doe",
+  age: 18,
+};
+console.log(firstname, reste1); // Affiche John { lastname: 'Doe', age: 18 }
