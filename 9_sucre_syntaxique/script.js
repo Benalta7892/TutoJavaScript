fd5fc197912b51f6f915f30b5ec98b8aaf1f1ce8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-26 17:48:42 +0200

    Sucre syntaxique

---
+++ 9_sucre_syntaxique/script.js
@@ -0,0 +1,13 @@
+// Description: Sucre syntaxique en JavaScript, les raccourcis pour écrire du code plus rapidement.
+let i = 0;
+// i = i +1
+// i++;
+
+// console.log(++i); // Affiche 1
+
+console.log(i++); // Affiche 0
+console.log(i); // Affiche 1
+
+let e = 3;
+e += 3;
+console.log(e); // Affiche 6
