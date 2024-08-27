3e8fd9409758f73271173dcbeae9e0457f59ec01
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 09:25:42 +0200

    Les timers

--- 10_timers/script.js
+++ 10_timers/script.js
@@ -3,5 +3,8 @@ function wait(duration) {
   while (Date.now() - t < duration) {}
 }
 console.log("Bonjour");
-wait(1000);
-console.log("Au revoir");
+// wait(1000); // Bloque le programme pendant 1 seconde
+setTimeout(() => {
+  console.log("Au revoir"); // Sera exécuté après 1 seconde
+}, 1000);
+console.log("Fin du programme"); // Sera exécuté avant "Au revoir"
