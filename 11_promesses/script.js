853d52d2c7e16513fcd4534bd853c9293d49d104
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 10:06:00 +0200

    Les promesses

--- 11_promesses/script.js
+++ 11_promesses/script.js
@@ -2,14 +2,27 @@ const p = new Promise((resolve, reject) => {
   resolve(4);
 });
 
-p.then((n) => {
-  console.log("Le nombre", n);
-  return 2;
-})
-  .then((n) => console.log("Le nombre 2", n))
-  .catch((e) => {
-    console.log("Erreur", e);
-    return 2;
+function wait(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(duration);
+    }, duration);
+  });
+}
+
+function waitAndFail(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(duration);
+    }, duration);
+  });
+}
+
+wait(2000)
+  .then(() => {
+    console.log("Attente 2s");
+    return wait(1000);
   })
-  .then((n) => console.log(2))
-  .finally(() => console.log("Fin")); // Sera exécuté dans tous les cas
+  .then(() => {
+    console.log("Attente 1s");
+  });
