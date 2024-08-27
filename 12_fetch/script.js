e076424ab45f25db11e2fa8a5828fa9956aa9855
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 11:58:46 +0200

    fetch

--- 12_fetch/script.js
+++ 12_fetch/script.js
@@ -1,19 +1,20 @@
-async function fetchUsers() {
-  const r = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    headers: { Accept: "application/json", "Content-Type": "application/json" },
-    body: JSON.stringify({ title: "Mon premier article" }),
+const a = new AbortController();
+
+Promise.race([
+  fetch("https://jsonplaceholder.typicode.com/posts/?_limit=5&_delay=2000", {
+    signal: a.signal,
+  }),
+
+  fetch("https://jsonplaceholder.typicode.com/users/?_limit=3", {
+    signal: a.signal,
+  }),
+])
+  .then((r) => r.json())
+  .then((body) => {
+    a.abort();
+    console.log(body);
   });
-  console.log(r.headers.get("content-Type"));
-  if (r.ok === true) {
-    // const data = await r.json();
-    return r.json();
-  }
-  throw new Error("Impossible de contacter le serveur");
-}
 
 // fetch("https://jsonplaceholder.typicode.com/ussssers")
 //   .then((r) => r.json())
 //   .then((body) => console.log(body));
-
-fetchUsers().then((users) => console.log(users)); // Erreur : Impossible de contacter le serveur
