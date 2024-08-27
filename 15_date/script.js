fa2a0219d7e4f9f1bd16c1ff5ffe92a5bfb14b94
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 17:24:17 +0200

    L'objet date

--- 15_date/script.js
+++ 15_date/script.js
@@ -4,24 +4,22 @@ function addDays(date, days) {
   return newDate;
 }
 
-const MONTHS = "months";
-const DAYS = "days";
-const YEARS = "years";
+const MONTHS = 1;
+const DAYS = 2;
+const YEARS = 0;
 
 function addInterval(date, n, unit) {
-  const newDate = new Date(date.getTime());
-  switch (unit) {
-    case DAYS:
-      newDate.setDate(newDate.getDate() + n);
-      break;
-    case MONTHS:
-      newDate.setMonth(newDate.getMonth() + n);
-      break;
-    case YEARS:
-      newDate.setFullYear(newDate.getFullYear() + n);
-      break;
-  }
-  return newDate;
+  const parts = [
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds(),
+  ];
+  parts[unit] += n;
+  return new Date(...parts);
 }
 
 const today = new Date();
