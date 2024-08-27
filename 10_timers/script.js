98ec1677c47baa0d27f9bc48b14fbf5b5e8be4a2
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 09:41:46 +0200

    Les timers

--- 10_timers/script.js
+++ 10_timers/script.js
@@ -1,21 +1,48 @@
-function wait(duration) {
-  const t = Date.now();
-  while (Date.now() - t < duration) {}
-}
-console.log("Bonjour");
-// wait(1000); // Bloque le programme pendant 1 seconde
-setTimeout(() => {
-  console.log("Au revoir"); // Sera exécuté après 1 seconde
-}, 1000);
-console.log("Fin du programme"); // Sera exécuté avant "Au revoir"
+// function wait(duration) {
+//   const t = Date.now();
+//   while (Date.now() - t < duration) {}
+// } // Bloque le programme pendant la durée duration
 
-function wait2(duration) {
-  const t2 = Date.now();
-  while (Date.now() - t < duration) {}
+// console.log("Bonjour");
+// // wait(1000); // Bloque le programme pendant 1 seconde
+// setTimeout(() => {
+//   console.log("Au revoir"); // Sera exécuté après 1 seconde
+// }, 1000);
+// console.log("Fin du programme"); // Sera exécuté avant "Au revoir"
+
+// const p = prompt("Entrez un nombre");
+
+// function wait2(duration) {
+//   const t2 = Date.now();
+//   while (Date.now() - t < duration) {}
+// }
+// console.log("Bonjour");
+// // wait(1000); // Bloque le programme pendant 1 seconde
+// const t2 = setInterval(() => {
+//   console.log("Au revoir"); // Sera exécuté après 1 seconde
+// }, 1000);
+// console.log(t2); // Sera exécuté avant "Au revoir"
+
+//Exercice
+// let i = 0;
+// const t = setInterval(() => {
+//   i++;
+//   console.log("Bonjour");
+//   if (i >= 5) {
+//     clearInterval(t);
+//   }
+// }, 1000);
+
+function decompte(n) {
+  console.log(n);
+  const t = setInterval(() => {
+    n--;
+    console.log(n);
+    if (n === 0) {
+      clearInterval(t);
+    }
+  }, 1000);
 }
-console.log("Bonjour");
-// wait(1000); // Bloque le programme pendant 1 seconde
-const t2 = setInterval(() => {
-  console.log("Au revoir"); // Sera exécuté après 1 seconde
-}, 1000);
-console.log(t2); // Sera exécuté avant "Au revoir"
+
+decompte(3);
+decompte(5);
