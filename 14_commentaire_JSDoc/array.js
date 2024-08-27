ac51d09152bb079c35e3fdc0ae324c84d7ebba71
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 16:41:12 +0200

    Commentaire et JSDoc

---
+++ 14_commentaire_JSDoc/array.js
@@ -0,0 +1,3 @@
+export { default as hello } from "./hello.js";
+
+export const sum = (items) => items.reduce((acc, item) => acc + item, 0);
---
+++ 14_commentaire_JSDoc/hello.js
@@ -0,0 +1,22 @@
+/**
+ * Permet de savoir si l'utilisateur peut conduire
+ *
+ * @param {number} age
+ * @param {string} country Code pays sur 2 caractères
+ * @returns {boolean}
+ */
+function canDrive(age, country) {
+  if (age >= 18) {
+    return true;
+    // La loi au US est que l'on peut conduire à partir de 16 ans
+  } else if (country === "US" && age >= 16) {
+    return true;
+  }
+  return false;
+}
+
+function isMajeur(age) {
+  return age >= 18;
+}
+
+const a = canDrive(18, "FR");
