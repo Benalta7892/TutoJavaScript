9f9c10f8aece48f5ec577638400c9f796dd7c1c5
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 09:50:33 +0200

    Les promesses

--- 11_promesses/script.js
+++ 11_promesses/script.js
@@ -1,7 +1,7 @@
 const p = new Promise((resolve, reject) => {
-  resolve(4);
+  reject(4);
 });
 
-p.then((n) => {
-  console.log("Le nombre est", n);
+p.catch((e) => {
+  console.log("Echec", e);
 });
