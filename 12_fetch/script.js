0601cae7a3c2cdba637793c9340aaeb5132861a0
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 11:19:33 +0200

    fetch

--- 12_fetch/script.js
+++ 12_fetch/script.js
@@ -1,3 +1,14 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((r) => r.json())
-  .then((body) => console.log(body));
+async function fetchUsers() {
+  const r = await fetch("https://jsonplaceholder.typicode.com/ussssers");
+  if (r.ok === true) {
+    const data = await r.json();
+    return r.json();
+  }
+  throw new Error("Impossible de contacter le serveur");
+}
+
+// fetch("https://jsonplaceholder.typicode.com/ussssers")
+//   .then((r) => r.json())
+//   .then((body) => console.log(body));
+
+fetchUsers().then((users) => console.log(users)); // Erreur : Impossible de contacter le serveur
