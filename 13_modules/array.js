9125a718c9d27febefb8af06126c1de54d5ee4e6
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-27 15:59:03 +0200

    Les modules

--- 13_modules/array.js
+++ 13_modules/array.js
@@ -1,5 +1,3 @@
-import hello2 from "./hello.js";
+export { default as hello } from "./hello.js";
 
 export const sum = (items) => items.reduce((acc, item) => acc + item, 0);
-
-export const hello = hello2;
