4bb10afb62839c36702fcc0031562b47d9625655
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-28 16:33:33 +0200

    JS navigateur

--- 16_JS_navigateur/2_DOM/app.js
+++ 16_JS_navigateur/2_DOM/app.js
@@ -1,5 +1,4 @@
-const li = document.querySelector("ul li:first-child");
+const li = document.querySelector("li");
 const ul = document.querySelector("ul");
-const div = document.createElement("div");
-div.innerHTML = "Je suis un div";
-ul.insertAdjacentElement("afterbegin", div);
+
+ul.append(li.cloneNode(true));
