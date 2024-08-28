60e9d16b9844803f91238a1194b02de5627cc1af
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-28 18:06:02 +0200

    Les écouteurs d'événements

--- 16_JS_navigateur/3_evenements/app.js
+++ 16_JS_navigateur/3_evenements/app.js
@@ -1,20 +1,4 @@
-/**
- *
- * @param {PointerEvent} event
- */
-function onButtonClick(event) {
-  console.log("button click");
-}
-
-document.querySelectorAll("button, a").forEach((btn) => {
-  btn.addEventListener("click", onButtonClick, { once: true });
+document.querySelector("form").addEventListener("submit", (e) => {
+  console.log(e);
+  e.preventDefault();
 });
-
-document.querySelector("div").addEventListener(
-  "click",
-  (e) => {
-    console.log("click div");
-    e.stopPropagation();
-  },
-  { capture: true }
-);
