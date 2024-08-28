152365f4b4707c7c85befd5acfea770dc315b5a8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-28 16:54:24 +0200

    JS navigateur

--- 16_JS_navigateur/2_DOM/app.js
+++ 16_JS_navigateur/2_DOM/app.js
@@ -1,4 +1,45 @@
-const li = document.querySelector("li");
-const ul = document.querySelector("ul");
+/**
+ * Crée un élément HTML représentant un article
+ * @param {{title: string, body: string}} post
+ * @returns {HTMLElement}
+ */
+function createArticle(post) {
+  const article = document.createElement("article");
+  article.innerHTML = `
+    <h2>${post.title}</h2>
+    <p>${post.body}</p>
+    `;
+  return article;
+}
 
-console.log(ul.contains(li)); // true
+async function main() {
+  const wrapper = document.querySelector("#lastPosts");
+  const loader = document.createElement("p");
+  loader.innerText = "Chargement en cours...";
+  wrapper.append(loader);
+  try {
+    const r = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?_limit=5",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    if (!r.ok) {
+      throw new Error("Erreur serveur");
+    }
+
+    const posts = await r.json();
+    loader.remove();
+    for (let post of posts) {
+      wrapper.append(createArticle(post));
+    }
+  } catch (e) {
+    loader.innerText = "Erreur lors du chargement";
+    loader.style.color = "red";
+    return;
+  }
+}
+
+main();
