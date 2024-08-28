d318897a0039a51c7f95ca2133d4c67c0136c792
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-28 18:26:54 +0200

    Les écouteurs d'événements

--- 16_JS_navigateur/3_evenements/app.js
+++ 16_JS_navigateur/3_evenements/app.js
@@ -1,3 +1,3 @@
-document.querySelector("input").addEventListener("change", (e) => {
-  console.log(e.currentTarget.checked);
+document.querySelector("select").addEventListener("change", (e) => {
+  console.log(Array.from(e.currentTarget.selectedOptions).map((o) => o.value));
 });
