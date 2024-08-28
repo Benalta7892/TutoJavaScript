82d42c8c894b635772999fa9c17205a58877816c
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-28 16:18:03 +0200

    JS navigateur

--- 16_JS_navigateur/2_DOM/app.js
+++ 16_JS_navigateur/2_DOM/app.js
@@ -1,5 +1,3 @@
-const ul = document.querySelector("ul li:first-child");
+const li = document.querySelector("ul li:first-child");
 
-setInterval(() => {
-  ul.classList.add("red");
-}, 1000);
+console.log(getComputedStyle(li).color);
