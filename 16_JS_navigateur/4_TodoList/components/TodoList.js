4c72cc377dc262e9cb83b42260e5bbf58304fbac
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-29 11:34:21 +0200

    TodoList

--- 16_JS_navigateur/4_TodoList/components/TodoList.js
+++ 16_JS_navigateur/4_TodoList/components/TodoList.js
@@ -92,10 +92,17 @@ class TodoListItem {
       class: "form-check-input",
       id,
     });
-    li.append(checkbox);
     const label = createElement("label", {
       class: "ms-2 form-check-label",
       for: id,
     });
+    label.innerText = todo.title;
+    const button = createElement("button", {
+      class: "ms-auto btn btn-danger btn-sm",
+    });
+    button.innerHTML = '<i class="bi-trash"> </i>';
+    li.append(checkbox);
+    li.append(label);
+    li.append(button);
   }
 }
