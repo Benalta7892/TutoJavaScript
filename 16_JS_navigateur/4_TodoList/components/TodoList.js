2997dba557f66fc4beab8e4a859afaa7daed11ec
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-08-29 15:15:46 +0200

    TodoList

--- 16_JS_navigateur/4_TodoList/components/TodoList.js
+++ 16_JS_navigateur/4_TodoList/components/TodoList.js
@@ -12,6 +12,10 @@ export class TodoList {
    * @type {Todo[]}
    */
   #todos = [];
+  /**
+   * @type {HTMLUListElement}
+   */
+  #listElement = [];
 
   /**
    * @param {Todo[]} todos
@@ -64,11 +68,32 @@ export class TodoList {
         <ul class="list-group">
         </ul>
       </main>`;
-    const list = element.querySelector(".list-group");
+    this.#listElement = element.querySelector(".list-group");
     for (let todo of this.#todos) {
       const t = new TodoListItem(todo);
-      t.appendTo(list);
+      this.#listElement.append(t.element);
     }
+    element
+      .querySelector("form")
+      .addEventListener("submit", (e) => this.onSubmit(e));
+  }
+  /**
+   * @param {SubmitEvent} e
+   */
+  onSubmit(e) {
+    e.preventDefault();
+    const title = new FormData(e.currentTarget).get("title").toString().trim();
+    if (title === "") {
+      return;
+    }
+
+    const todo = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    const item = new TodoListItem(todo);
+    this.#listElement.prepend(item.element);
   }
 }
 
@@ -109,10 +134,10 @@ class TodoListItem {
 
   /**
    *
-   * @param {HTMLElement} element
+   * @return {HTMLElement}
    */
-  appendTo(element) {
-    element.append(this.#element);
+  get element() {
+    return this.#element;
   }
 
   /**
