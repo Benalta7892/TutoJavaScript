78522d94566c7ff53523ccbf79029478383295b9
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-02 17:28:49 +0200

    Intersection Observer

--- 16_JS_navigateur/11_intersectionObserver/app.js
+++ 16_JS_navigateur/11_intersectionObserver/app.js
@@ -1,15 +1,20 @@
-const observer = new IntersectionObserver(
-  (entries) => {
-    for (const entry of entries) {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("is-visible");
-      }
+const observer = new IntersectionObserver((entries) => {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+      entry.target.animate(
+        [
+          { transform: "translateX(-50px)", opacity: 0 },
+          { transform: "translateX(0)", opacity: 1 },
+        ],
+        {
+          duration: 300,
+        }
+      );
+      observer.unobserve(entry.target);
     }
-  },
-  {
-    threshold: 0.5,
   }
-);
+});
 
 observer.observe(document.querySelector(".btn1"));
 observer.observe(document.querySelector(".btn2"));
