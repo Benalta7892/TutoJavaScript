1c9f08078c3ad9ef63cfe435a6d5fafae9a25de5
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-02 16:44:43 +0200

    Fonctions usuelles dans le navigateur

--- 16_JS_navigateur/10_fonctions_usuelles_navigateur/app.js
+++ 16_JS_navigateur/10_fonctions_usuelles_navigateur/app.js
@@ -1,14 +1,4 @@
-const h1 = document.querySelector("h1");
-console.log("Position par rapport au haut", window.scrollY + h1.getBoundingClientRect().y, recursiveOffsetTop(h1));
-
-document.querySelector("div").addEventListener("mousemove", (e) => {
-  console.log(e.offsetX, e.pageX);
-});
-
-function recursiveOffsetTop(element) {
-  if (element.offsetParent) {
-    return element.offsetTop + recursiveOffsetTop(element.offsetParent);
-  } else {
-    return element.offsetTop;
-  }
-}
+const div = document.querySelector("div");
+console.log(div.dataset.user);
+div.dataset.hello = "Bonjour";
+console.log(div.dataset);
