fad503cc6d8a9396dcd6f1149754226e1954fb1f
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-02 15:11:56 +0200

    Manipuler les cookies

--- 16_JS_navigateur/8_cookies/app.js
+++ 16_JS_navigateur/8_cookies/app.js
@@ -1,1 +1,29 @@
-// Visionnage du contenu de ce chapitre sur la vidéo du cours
+/**
+ *
+ * @param {string} name
+ * @return {string|null}
+ */
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  const value = cookies.find((c) => c.startsWith(name))?.split("=")[1];
+  if (value === undefined) {
+    return null;
+  }
+  return decodeURIComponent(value);
+}
+
+/**
+ *
+ * @param {string} name
+ * @param {string} value
+ * @param {number} days
+ */
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setDate(date.getDate() + days);
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${date.toUTCString()}`;
+}
+
+// setCookie("hello", "Bonjour les gens", 2);
+
+console.log(getCookie("hello"));
