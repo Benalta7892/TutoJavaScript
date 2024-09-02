cde0b039a0ff1a508a6a3ca2a923f81ac2af0cda
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-02 16:48:38 +0200

    Fonctions usuelles dans le navigateur

--- 16_JS_navigateur/10_fonctions_usuelles_navigateur/app.js
+++ 16_JS_navigateur/10_fonctions_usuelles_navigateur/app.js
@@ -1,4 +1,10 @@
-const div = document.querySelector("div");
-console.log(div.dataset.user);
-div.dataset.hello = "Bonjour";
-console.log(div.dataset);
+const button = document.querySelector("button");
+let i = 0;
+const listener = () => {
+  i++;
+  console.log(button.dataset.name);
+  if (i >= 3) {
+    button.removeEventListener("click", listener);
+  }
+};
+button.addEventListener("click", listener);
