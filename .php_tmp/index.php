69be88b232c025027d5030bda9b3274ce250dc2a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-03 11:20:17 +0200

    Vite

---
+++ .php_tmp/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php
+  $dev = true;
+  ?>
+  <script src="http://localhost:5173/@vite/client" type="module"></script>
+  <script src="http://localhost:5173/resources/main.js" type="module"></script>
+</head>
+
+<body>
+  <p>Ceci est mon site php <?= date('d/m/Y') ?> </p>
+</body>
+
+</html>
