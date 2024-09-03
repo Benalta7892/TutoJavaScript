d1c74b5672c0c0b45d4b13d169d63ca5ae6a8547
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-03 14:51:00 +0200

    Vite

---
+++ 16_JS_navigateur/13_vite/viteproject/server/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php
+  $dev = true;
+  ?>
+</head>
+
+<body>
+  <p>Ceci est mon site <?= date('d/m/Y') ?></p>
+</body>
+
+</html>
