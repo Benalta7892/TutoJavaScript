5c2c70021dd473fc9331c45c8dab67f3720b62c0
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-03 12:47:16 +0200

    Vite

--- 16_JS_navigateur/13_vite/vite-project/server/vite.config.js
+++ 16_JS_navigateur/13_vite/vite-project/server/vite.config.js
@@ -3,9 +3,13 @@ import { defineConfig } from "vite";
 // vite.config.js
 export default defineConfig({
   server: {
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
   },
+  base: "/assets/",
   build: {
+    copyPublicDir: false,
+    outDir: "public/assets",
+    assetsDir: "",
     // generate .vite/manifest.json in outDir
     manifest: true,
     rollupOptions: {
