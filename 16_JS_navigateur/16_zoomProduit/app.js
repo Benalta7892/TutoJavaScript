6a7a0affd8574bc7be8c85c5cfbc389395535f34
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-05 15:25:16 +0200

    Zoom Produit

--- 16_JS_navigateur/16_zoomProduit/app.js
+++ 16_JS_navigateur/16_zoomProduit/app.js
@@ -1,6 +1,8 @@
 class ProductViewer {
   #mediumImage;
+  #largeImage;
   #thumbnailWrapper;
+  #zoomElement;
 
   /**
    * @param {HTMLElement} element
@@ -8,10 +10,14 @@ class ProductViewer {
   constructor(element) {
     this.#mediumImage = element.querySelector(".js-image-medium");
     this.#thumbnailWrapper = element.querySelector(".js-images");
+    this.#zoomElement = element.querySelector(".js-zoom");
+    this.#largeImage = element.querySelector(".js-image-large");
+
     const links = this.#thumbnailWrapper.querySelectorAll("a");
     for (const link of links) {
       link.addEventListener("click", this.#onThumbnailClick.bind(this));
     }
+    element.querySelector(".js-image-medium").addEventListener("mouseenter", this.#onEnter.bind(this));
   }
 
   /**
@@ -23,6 +29,14 @@ class ProductViewer {
     e.currentTarget.classList.add("active");
     const medium = e.currentTarget.dataset.medium;
     this.#mediumImage.src = medium;
+  }
+
+  /**
+   *
+   * @param {PointerEvent} e
+   */
+  #onEnter(e) {
+    this.#zoomElement.classList.add("active");
   }
 }
 
