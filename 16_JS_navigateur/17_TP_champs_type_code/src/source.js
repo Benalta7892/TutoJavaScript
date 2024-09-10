8d1e96178df38116e3ca37a9d3ffe648a1d76e50
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-10 16:21:19 +0200

    champs type code

---
+++ 16_JS_navigateur/17_TP_champs_type_code/src/source.js
@@ -0,0 +1,37 @@
+/**
+ * Elément web personnalisé
+ * Plus d'info : https://grafikart.fr/tutoriels/web-component-1201
+ */
+class CodeInput extends HTMLElement {
+  static get observedAttributes() {
+    return ["value"];
+  }
+
+  connectedCallback() {
+    const legend = this.getAttribute("legend") ?? "Entrez votre code";
+    const name = this.getAttribute("name") ?? "";
+    const size = parseInt(this.getAttribute("size") ?? "6", 10);
+    this.innerHTML = `
+      <fieldset>
+        <legend>${legend}</legend>
+        <div class="code-inputs">
+          ${Array.from(
+            { length: size },
+            (_, k) => `<input type="text" inputmode="numeric" aria-label="Chiffre ${k}" pattern="[0-9]{1}" />`
+          ).join("")}
+        </div>
+        <input type="hidden" name"${name}" />
+      </fieldset>`;
+  }
+
+  /**
+   * Code exécuté lorsqu'un attribut change
+   *
+   * @param name Nom de l'attribut
+   * @param oldValue Ancienne valeur
+   * @param newValue Nouvelle valeur
+   */
+  attributeChangedCallback(name, oldValue, newValue) {}
+}
+
+customElements.define("code-input", CodeInput);
