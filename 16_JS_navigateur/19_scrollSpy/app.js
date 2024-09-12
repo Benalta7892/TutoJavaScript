f7e576c35973eb6fda804fda67724f450dfa2720
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-12 11:29:03 +0200

    ScrollSpy

---
+++ 16_JS_navigateur/19_scrollSpy/app.js
@@ -0,0 +1,33 @@
+/**
+ * @param {HTMLElement} elem
+ */
+const activate = function (elem) {
+  const id = elem.getAttribute("id");
+  const anchor = document.querySelector(`a[href="#${id}"]`);
+  if (anchor === null) {
+    return null;
+  }
+  anchor.parentElement.querySelectorAll(".active").forEach((node) => node.classList.remove("active"));
+  anchor.classList.add("active");
+};
+
+/**
+ * @param {IntersectionObserverEntry[]} entries
+ * @param {IntersectionObserver} observer
+ */
+const callback = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > 0) {
+      activate(entry.target);
+    }
+  });
+};
+
+const spies = document.querySelectorAll("[data-spy]");
+
+if (spies.length > 0) {
+  const observer = new IntersectionObserver(callback, {});
+  spies.forEach(function (spy) {
+    observer.observe(spy);
+  });
+}
---
+++ 16_JS_navigateur/19_scrollSpy/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="app.js" defer></script>
+</head>
+
+<body>
+  <nav>
+    <?php for ($i = 1; $i <= 4; $i++): ?>
+      <a href="#section<?= $i ?>">Section <?= $i ?></a>
+    <?php endfor ?>
+    <a href="constact.html">Contact</a>
+  </nav>
+  <main>
+    <?php for ($i = 1; $i <= 4; $i++): ?>
+      <section id="section<?= $i ?>" data-spy>
+        Section <?= $i ?>
+      </section>
+    <?php endfor ?>
+  </main>
+</body>
+
+</html>
