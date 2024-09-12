a433e0902dab92623a7a9f6642a7ba2e1bcafa7e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-12 21:24:16 +0200

    Creer un Carousel

---
+++ 16_JS_navigateur/21_carousel/js/main.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+class Carousel {
+  /**
+   *
+   * @param {HTMLElement} element
+   * @param {Object} options
+   * @param {Object} options.slidesToScroll Nombre d'éléments à faire défiler
+   * @param {Object} options.slidesVisible Nombre d'éléments visible dans un slide
+   */
+  constructor(element, options = {}) {}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Carousel(document.querySelector("#carousel1"), {
+    slidesToScroll: 3,
+    slidesVisible: 3,
+  });
+});
