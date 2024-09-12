7edbc7f606d26a75670cc7ba412cd67f3e68102c
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-12 22:06:39 +0200

    Creer un Carousel

--- 16_JS_navigateur/21_carousel/js/main.js
+++ 16_JS_navigateur/21_carousel/js/main.js
@@ -21,7 +21,6 @@ class Carousel {
 
 document.addEventListener("DOMContentLoaded", function () {
   new Carousel(document.querySelector("#carousel1"), {
-    slidesToScroll: 3,
     slidesVisible: 3,
   });
 });
