7ebc8f0fde8718776697983a02000538e99343d3
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-13 19:20:00 +0200

    Lire et ecrire des fichiers

--- 17_JS_serveur/2_lire_ecrire_fichiers/app.js
+++ 17_JS_serveur/2_lire_ecrire_fichiers/app.js
@@ -1,11 +1,17 @@
 import { readdir, stat } from "node:fs/promises";
 
+const wait = (duration) => new Promise((resolve) => setTimeout(resolve, duration));
+
+console.time("code");
 const files = await readdir("./", { withFileTypes: true });
-for (const file of files) {
-  const parts = [file.isDirectory() ? "D" : "F", file.name];
-  if (!file.isDirectory()) {
-    const { size } = await stat(file.name);
-    parts.push(`${size}o`);
-  }
-  console.log(parts.join(" - "));
-}
+await Promise.allSettled(
+  files.map(async (file) => {
+    const parts = [file.isDirectory() ? "D" : "F", file.name];
+    if (!file.isDirectory()) {
+      const { size } = await stat(file.name);
+      parts.push(`${size}o`);
+    }
+    console.log(parts.join(" - "));
+  })
+);
+console.timeEnd("code");
