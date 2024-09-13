3a28f460b82a0e9d9d3c93c695fdbb94a7856e01
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-13 10:48:58 +0200

    Creer un Carousel

--- 16_JS_navigateur/21_carousel/js/main.js
+++ 16_JS_navigateur/21_carousel/js/main.js
@@ -16,22 +16,54 @@ class Carousel {
       },
       options
     );
-    this.children = [].slice.call(element.children);
-    let root = this.createDivWithClass("carousel");
+    let children = [].slice.call(element.children);
+    this.currentItem = 0;
+    this.root = this.createDivWithClass("carousel");
     this.container = this.createDivWithClass("carousel_container");
-    root.appendChild(this.container);
-    this, element.appendChild(root);
-    this.children.forEach((child) => {
+    this.root.appendChild(this.container);
+    this.element.appendChild(this.root);
+    this.items = children.map((child) => {
       let item = this.createDivWithClass("carousel_item");
-      item.style.width = 100 / this.options.slidesVisible / ratio + "%";
       item.appendChild(child);
       this.container.appendChild(item);
+      return item;
     });
+    this.setStyle();
+    this.createNavigation();
   }
 
+  /**
+   * Applique les bonnes dimensions aux éléments du carousel
+   */
   setStyle() {
-    let ratio = this.children.length / this.options.slidesVisible;
+    let ratio = this.items.length / this.options.slidesVisible;
     this.container.style.width = ratio * 100 + "%";
+    this.items.forEach((item) => (item.style.width = 100 / this.options.slidesVisible / ratio + "%"));
+  }
+
+  createNavigation() {
+    let nextButton = this.createDivWithClass("carousel_next");
+    let prevButton = this.createDivWithClass("carousel_prev");
+    this.root.appendChild(nextButton);
+    this.root.appendChild(prevButton);
+    nextButton.addEventListener("click", this.next.bind(this));
+    prevButton.addEventListener("click", this.prev.bind(this));
+  }
+
+  next() {
+    this.goToItem(this.currentItem + this.options.slidesToScroll);
+  }
+
+  prev() {
+    this.goToItem(this.currentItem - this.options.slidesToScroll);
+  }
+
+  /**
+   * Déplace le carousel vers l'élément ciblé
+   * @param {number} index
+   */
+  goToItem(index) {
+    this.currentItem = index;
   }
 
   /**
