2d8c55f8c9fc7bda01db58c3967dee680c1268cd
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-14 16:30:21 +0200

    Utiliser npm

--- 17_JS_serveur/7_npm/app.js
+++ 17_JS_serveur/7_npm/app.js
@@ -1,3 +1,21 @@
-import chalk from "chalk";
+import Fastify from "fastify";
+import { findTodos } from "./functions/todos_storage.js";
+import { removeTodo } from "./functions/todos_storage.js";
 
-console.log(chalk.blue("Bonjour"));
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/todos", async (request, reply) => {
+  reply.send(await findTodos());
+});
+
+fastify.delete("/todos", async (request, reply) => {
+  await removeTodo(parseInt(request.query.id, 10));
+  reply.code(204);
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err;
+  // Server is now listening on ${address}
+});
