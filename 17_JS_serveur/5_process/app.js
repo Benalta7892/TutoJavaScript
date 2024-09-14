83920fb67ffde748ed6b2e95bf791c3f99378231
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-14 12:36:06 +0200

    Process

--- 17_JS_serveur/5_process/app.js
+++ 17_JS_serveur/5_process/app.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+import { createServer } from "http";
+
+createServer((req, res) => {
+  res.write("Bonsoir");
+  console.log("Bonjour");
+  res.end();
+}).listen("80");
--- 17_JS_serveur/5_process/watcher.js
+++ 17_JS_serveur/5_process/watcher.js
@@ -1,16 +1,31 @@
 import { exec, spawn } from "node:child_process";
+import { watch } from "node:fs/promises";
 
 const [node, _, file] = process.argv;
 
-const pr = spawn(node, [file]);
-pr.stdout.on("data", (data) => {
-  console.log(data.toString("utf8"));
-});
+function spawnNode() {
+  const pr = spawn(node, [file]);
+  pr.stdout.on("data", (data) => {
+    console.log(data.toString("utf8"));
+  });
 
-pr.stderr.on("data", (data) => {
-  console.error(data.toString("utf8"));
-});
+  pr.stderr.on("data", (data) => {
+    console.error(data.toString("utf8"));
+  });
 
-pr.on("close", (code) => {
-  console.log("Process exited: " + code);
-});
+  pr.on("close", (code) => {
+    if (code > 0) {
+      throw new Error("Process exited : " + code);
+    }
+  });
+  return pr;
+}
+
+let childNodeProcess = spawnNode();
+const watcher = watch("./", { recursive: true });
+for await (const event of watcher) {
+  if (event.filename.endsWith(".js")) {
+    childNodeProcess.kill();
+    childNodeProcess = spawnNode();
+  }
+}
