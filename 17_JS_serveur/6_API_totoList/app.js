8ec4726712123b2a704284cb910271dc077d646d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-14 14:10:36 +0200

    API TodoList

--- 17_JS_serveur/6_API_totoList/app.js
+++ 17_JS_serveur/6_API_totoList/app.js
@@ -1,13 +1,18 @@
 import { createServer } from "node:http";
 import { findTodos } from "./functions/todos_storage.js";
+import { json } from "node:stream/consumers";
 
 createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
   const url = new URL(req.url, `http://${req.headers.host}`);
   if (url.pathname === "/todos") {
-    console.log(url);
-    const todos = await findTodos();
-    res.write(JSON.stringify(todos));
+    if (req.method === "GET") {
+      const todos = await findTodos();
+      res.write(JSON.stringify(todos));
+    } else if (req.method === "POST") {
+      const newTodo = await json(req);
+      console.log(newTodo);
+    }
   } else {
     res.writeHead(404);
   }
