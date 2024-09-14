169b8c612e206e4d97e2020e48b236d2fe32287e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-14 11:36:30 +0200

    Serveur web avec node.js

--- 17_JS_serveur/4_serveur_web/app.js
+++ 17_JS_serveur/4_serveur_web/app.js
@@ -1,17 +1,12 @@
 // npm init -y , pour créer le fichier package.json, et rajouter "type": "module" pour activer les modules ES6
 import { createReadStream } from "node:fs";
 import { createServer } from "node:http";
+import { text, json } from "node:stream/consumers";
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-  req.on("close", () => {
-    console.log(body);
-    res.end();
-  });
+  res.write(`Bonjour ${(await json(req)).name}`);
+  res.end();
   // res.write(`Bonjour ${url.searchParams.get("name")}`);
 });
 server.listen("80");
