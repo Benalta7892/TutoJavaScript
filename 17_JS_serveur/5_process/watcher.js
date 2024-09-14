df05ab0a33ccaa834d39c41e4b54939b01d4b79b
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-14 12:23:30 +0200

    Process

--- 17_JS_serveur/5_process/watcher.js
+++ 17_JS_serveur/5_process/watcher.js
@@ -2,7 +2,7 @@ import { exec, spawn } from "node:child_process";
 
 const [node, _, file] = process.argv;
 
-const pr = spawn("ls");
+const pr = spawn(node, [file]);
 pr.stdout.on("data", (data) => {
   console.log(data.toString("utf8"));
 });
