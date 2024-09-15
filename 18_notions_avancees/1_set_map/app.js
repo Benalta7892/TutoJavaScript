b5e8d7d0110dcfca4083d0fc9b4a16686b3b7504
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-15 10:38:56 +0200

    Set et Map

--- 18_notions_avancees/1_set_map/app.js
+++ 18_notions_avancees/1_set_map/app.js
@@ -1,3 +1,19 @@
-const names = ["John", "Jane", "Henry", "Madeleine", "John"];
+const persons = [
+  { name: "John Doe", age: 18 },
+  { name: "Jane Doe", age: 28 },
+  { name: "Henry Doe", age: 32 },
+];
 
-console.log(Array.from(new Set(names)));
+const ul = document.createElement("ul");
+
+const liMap = new Map();
+
+persons.forEach((person) => {
+  const li = document.createElement("li");
+  li.innerHTML = `<strong>${person.name}</strong>: ${person.age}`;
+  ul.appendChild(li);
+  liMap.set(person, li);
+});
+
+// Je veux le li correspondant à la personne
+console.log(liMap.get(persons[1])); // <li><strong>Jane Doe</strong>: 28</li>
