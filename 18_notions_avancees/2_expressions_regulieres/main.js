9244e4f1e9656a2cf4d1f4aa0d2d47a76d22cffe
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-15 11:09:38 +0200

    Les expressions regulieres

--- 18_notions_avancees/2_expressions_regulieres/main.js
+++ 18_notions_avancees/2_expressions_regulieres/main.js
@@ -1,3 +1,16 @@
-const expr = /([a-z]{3,})/g;
+const expr = /(\d{2}):(\d{2})/g;
+const description = `
+00:00 Introduction
+01:00 Les symboles
+02:00 Les expressions régulières
+03:00 Les fonctions
+04:00 Les objets
+05:00 Les tableaux
+06:00 Les boucles
+07:00 Les conditions
+08:00 Les classes
+09:00 Les modules
+10:00 Conclusion
+`;
 
-console.log("bonjour les gens".match(expr)); // ["bonjour", "bonjour"]
+console.log(description.match(expr)); // ["bonjour", "bonjour"]
