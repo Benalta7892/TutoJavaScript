238b1995c2d4a86bc0b3106c4f3e948b4b01232d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-09-15 11:48:25 +0200

    Decorer un objet avec les proxy

--- 18_notions_avancees/3_objet_proxy/main.js
+++ 18_notions_avancees/3_objet_proxy/main.js
@@ -1,17 +1,39 @@
-const a = {
-  b: 3,
+// const a = {
+//   b: 3,
+// };
+
+// const p = new Proxy(a, {
+//   get(target, prop, receiver) {
+//     console.log("Accès a une propriété", prop);
+//     return Reflect.get(...arguments);
+//   },
+//   set(target, prop, value, receiver) {
+//     console.log("Ecriture dans la propriete", prop, value);
+//     return Reflect.set(...arguments);
+//   },
+// });
+
+// p.b = 2;
+// console.log(a);
+
+const Jhon = {
+  name: "Jhon Doe",
+  age: 20,
 };
 
-const p = new Proxy(a, {
-  get(target, prop, receiver) {
-    console.log("Accès a une propriété", prop);
-    return Reflect.get(...arguments);
-  },
+function updateUser(person) {
+  person.age = 3;
+}
+
+const johnMajeur = new Proxy(Jhon, {
   set(target, prop, value, receiver) {
-    console.log("Ecriture dans la propriete", prop, value);
+    if (prop === "age" && value < 18) {
+      throw new Error("La personne ne peut pas devenir mineur");
+    }
     return Reflect.set(...arguments);
   },
 });
 
-p.b = 2;
-console.log(a);
+// Mon code
+updateUser(johnMajeur);
+console.log(johnMajeur);
